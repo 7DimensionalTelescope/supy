5f6914243d75221d3d32c570f323f48660365bb4
Won-Hyeong Lee <wohy1220@gmail.com>
2026-04-14 15:15:40 +0900

    Add: add Celealt module, merged and replace observer module later

---
+++ supy/celearlt/__init__.py
@@ -0,0 +1,7 @@
+"""
+WRITER: Hyeonho Choi (hhchoi1022@gmail.com)
+MODIFIER: Won-Hyeong Lee (wohy1220@gmail.com)
+DATE: 2026-03-23
+DESCRIPTION: This module is a standalone module for the targets class. It is used to calculate the rise, transit, and set times of the targets for each day of the specified year.
+ALL CALCULATION IS BASED ON astroplan library.
+"""
